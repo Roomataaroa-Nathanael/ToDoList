23efd4b7c961de0fb03d42d3000c8ac053a3be16
ROOMATAAROA Nathanaël <nathanael.roomataaroa@gmail.com>
2025-04-01 22:48:17 -1000

    Update database_script.sql
    - added test data

--- Database/database_script.sql
+++ Database/database_script.sql
@@ -2,17 +2,23 @@
 -- Database : toDoList_databse
 --
 --
------ Structure of the 'Repository' table -----
+DROP TABLE IF EXISTS belongs_to;
+DROP TABLE IF EXISTS Task;
 DROP TABLE IF EXISTS Repository;
+DROP TABLE IF EXISTS Category;
+
+----- Structure of the 'Repository' table -----
 CREATE TABLE Repository (
     id_repository BIGINT AUTO_INCREMENT NOT NULL,
     name_repository VARCHAR(255) NOT NULL,
     PRIMARY KEY (id_repository)
 );
 -----------------------------------------------
+-- Insert into the 'Repository' table
+INSERT INTO Repository (name_repository) 
+VALUES ('Personal Tasks');
 
 ----- Structure of the 'Task' table -----------
-DROP TABLE IF EXISTS Task;
 CREATE TABLE Task (
     id_task BIGINT AUTO_INCREMENT NOT NULL,
     name_task TEXT NOT NULL,
@@ -25,9 +31,17 @@ CREATE TABLE Task (
         REFERENCES Repository (id_repository) ON DELETE CASCADE
 );
 ----------------------------------------------
+-- Insert into the 'Task' table
+INSERT INTO Task (name_task, state_task, deadline_task, priority_task, id_repository) 
+VALUES ('Clean the house', 'not done', '2025-04-10 10:00:00', 1, 1); -- id_repository = 1 (Personal Tasks)
+
+INSERT INTO Task (name_task, state_task, deadline_task, priority_task, id_repository) 
+VALUES ('Finish project report', 'in progress', '2025-04-15 17:00:00', 2, 1); -- id_repository = 2 (Work Tasks)
+
+INSERT INTO Task (name_task, state_task, deadline_task, priority_task, id_repository) 
+VALUES ('Buy groceries', 'not done', '2025-04-05 12:00:00', 3, 1); -- id_repository = 1 (Personal Tasks)
 
 ----- Structure of the 'Category' table ------
-DROP TABLE IF EXISTS Category;
 CREATE TABLE Category (
     id_category BIGINT AUTO_INCREMENT NOT NULL,
     name_category VARCHAR(255) NOT NULL,
@@ -35,9 +49,17 @@ CREATE TABLE Category (
     PRIMARY KEY (id_category)
 );
 ----------------------------------------------
+-- Insert into the 'Category' table
+INSERT INTO Category (name_category, description_category) 
+VALUES ('Household', 'Tasks related to household chores');
+
+INSERT INTO Category (name_category, description_category) 
+VALUES ('Work', 'Tasks related to professional work');
+
+INSERT INTO Category (name_category, description_category) 
+VALUES ('Groceries', 'Tasks related to grocery shopping');
 
 ----- Structure of the 'belongs_to' table ----
-DROP TABLE IF EXISTS belongs_to;
 CREATE TABLE belongs_to (
     id_task BIGINT NOT NULL,
     id_category BIGINT NOT NULL,
@@ -48,3 +70,15 @@ CREATE TABLE belongs_to (
         REFERENCES Category (id_category) ON DELETE CASCADE
 );
 ----------------------------------------------
+-- Insert into the 'belongs_to' table
+INSERT INTO belongs_to (id_task, id_category) 
+VALUES (1, 1); 
+
+INSERT INTO belongs_to (id_task, id_category) 
+VALUES (2, 2); 
+
+INSERT INTO belongs_to (id_task, id_category) 
+VALUES (3, 3);
+
+INSERT INTO belongs_to (id_task, id_category) 
+VALUES (1, 3); 
